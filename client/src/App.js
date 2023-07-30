efa0716ff256f8c1aba46ec8b18559964453dfc6
Tran Trong Dinh <trantrongdinh0221@gmail.com>
2023-07-30 12:56:06 +0700

    search suggestions

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import Map from "./components/Map/Map";
 
 axios.defaults.baseURL = "http://localhost:5000";
 axios.defaults.withCredentials = true;
+
 function App() {
   return (
     <BrowserRouter>
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -7,12 +7,58 @@ import Search from "./Search/Search";
 import { Context } from "../../utils/context";
 import Cart from "../Cart/Cart";
 import logo from "../../assets/logo.png";
+import { SearchResultsList } from "./Search/SearchResultsList";
 
 const Header = () => {
+  const [results, setResults] = useState([]);
+  const [input, setInput] = useState("");
   const { user } = useContext(Context);
   const [scrolled, setScrolled] = useState(false);
   const [searchModal, setSearchModal] = useState(false);
   const navigate = useNavigate();
+
+  const fetchData = (value) => {
+    // Giả sử đã có một mảng cố định chứa danh sách người dùng
+    const categoryArray = [
+      { id: 1, name: "Gundam Model Kits" },
+      { id: 2, name: "One Piece Model Kits" },
+      { id: 3, name: "Dragon Ball Model Kits" },
+      { id: 4, name: "Evangelion Model Kits" },
+      { id: 5, name: "Naruto Model Kits" },
+      { id: 6, name: "My Hero Academia Model Kits" },
+      { id: 7, name: "Attack on Titan Model Kits" },
+      { id: 8, name: "Studio Ghibli Model Kits" },
+      { id: 9, name: "Figure-rise Standard" },
+      { id: 10, name: "PlaMo (Plastic Model)" },
+      { id: 11, name: "Bandai Hobby Kits" },
+      { id: 12, name: "Kotobukiya Model Kits" },
+      { id: 13, name: "Good Smile Company Model Kits" },
+      { id: 14, name: "Figma Model Kits" },
+      { id: 15, name: "Nendoroid Model Kits" },
+      { id: 16, name: "Model Kit Customization" },
+      { id: 17, name: "Weathering and Painting Techniques" },
+      { id: 18, name: "Kit Bashing" },
+      { id: 19, name: "Anime Model Photography" },
+      // Thêm các đối tượng người dùng khác vào đây...
+    ];
+
+    // Lọc danh sách người dùng dựa trên giá trị `value`
+    const results = categoryArray.filter((user) => {
+      return (
+        value &&
+        user &&
+        user.name &&
+        user.name.toLowerCase().includes(value.toLowerCase())
+      );
+    });
+
+    // Tiếp tục xử lý kết quả (ở đây, ta giả sử có hàm setResults để lưu trữ kết quả)
+    setResults(results);
+  };
+  const handleChange = (value) => {
+    setInput(value);
+    fetchData(value);
+  };
   const handleScroll = () => {
     const offset = window.scrollY;
     if (offset > 200) {
@@ -37,9 +83,8 @@ const Header = () => {
           </div>
           <ul className="center">
             <li onClick={() => navigate("/")}>Home</li>
-            <li onClick={() => navigate("/about")}>About</li>
+            <li onClick={() => navigate("/category")}>Products</li>
             <li onClick={() => navigate("/contact")}>Contact</li>
-            <li>Products</li>
           </ul>
 
           <div className="right">
@@ -48,7 +93,11 @@ const Header = () => {
                 type="text"
                 placeholder="Search"
                 className="search-input"
+                value={input}
+                onChange={(e) => handleChange(e.target.value)}
               />
+
+              <SearchResultsList results={results} />
               <TbSearch onClick={() => setSearchModal(true)} />
             </div>
 
